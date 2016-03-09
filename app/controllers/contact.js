be7020f9530c6be44d148bf484a4350112c4db0d
Daniel T <danthwa@gmail.com>
2016-03-08 20:51:30 -0800

    implementing firebase

--- app/controllers/contact.js
+++ app/controllers/contact.js
@@ -2,20 +2,23 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
 
-    troll : 'are you mad?',
+    emailAddress: '',
+
+    message: '',
+
+    validEmail: Ember.computed.match('emailAddress', /^.+@.+\..+$/),
+    validMessage: Ember.computed.gte('message.length', 5),
+
+    isValid: Ember.computed.and('validMessage', 'validEmail'),
+    isDisabled: Ember.computed.not('isValid'),
 
     mad : undefined,
-
+    troll : 'are you mad?',
     userMadnessBinary : Ember.observer('mad', function(){
 
-        console.log('mad changed to', this.get('mad'));
-
-        if(this.get('mad') === true){
-
+        if(this.get('mad')){
             this.set('troll', 'why you so mad bro?');
-
         } else {
-
             this.set('troll', 'Oh OK, well that\'s good. Are you sure you aren\'t mad?');
         }
     })
