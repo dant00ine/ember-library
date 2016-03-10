9da025545ad636ffec05f4fbef1b9387f7c26f5e
Daniel T <danthwa@gmail.com>
2016-03-09 23:52:03 -0800

    added edit and delete functionality for all content, began writing css

--- app/routes/admin/messages.js
+++ app/routes/admin/messages.js
@@ -1,7 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  model(){
+  model() {
     return this.store.findAll('contact');
+  },
+
+  actions : {
+    delete(message){
+      message.destroyRecord();
+    }
   }
 });
