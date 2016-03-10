a4e5b5ed7795e3f827c21f3328fca46867a145a9
Daniel T <danthwa@gmail.com>
2016-03-10 11:03:25 -0800

    converted more logic to routes, refined code with reusable form and listing components

--- app/routes/contact.js
+++ app/routes/contact.js
@@ -4,5 +4,42 @@ export default Ember.Route.extend({
 
   model(){
     this.store.findAll('contact');
-  }
+  },
+
+//      init() {
+//        debugger;
+//      },
+//
+//      beforeModel(transition) {
+//        console.log("HERE IS THE TRANSITION", transition);
+//        debugger;
+//      },
+//
+//      model(params, transition) {
+//        console.log(params);
+//        console.log(transition);
+//        debugger;
+//      },
+//
+//      afterModel(model, transition) {
+//        console.log(model);
+//        console.log(transition);
+//        debugger;
+//      },
+//
+//      activate() {
+//        debugger;
+//      },
+//
+//      setupController(controller, model) {
+//        console.log(controller);
+//        console.log(transition);
+//        debugger;
+//      },
+//
+//      renderTemplate(controller, model) {
+//        console.log(controller);
+//        console.log(model);
+//        debugger;
+//      }
 });
